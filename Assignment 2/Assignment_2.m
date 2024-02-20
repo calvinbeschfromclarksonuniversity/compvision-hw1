7a25e43ef2b207882ac8e4948005fc50356c8287
Trevor <trevoratorsavage1@gmail.com>
2024-02-20 16:08:23 -0500

    part b done

--- Assignment 2/Assignment_2.m
+++ Assignment 2/Assignment_2.m
@@ -1,21 +1,24 @@
+im1 = imread("Image1.jpg");
+im2 = imread("Image2.jpg");
 
- im1 = im2double(InputImage);
- im1 = rgb2gray(InputImage);
- im2 = im2double(InputImage);
- im2 = rgb2gray(InputImage);
+im1 = rgb2gray(im1);
+im1 = im2double(im1);
+
+im2 = rgb2gray(im2);
+im2 = im2double(im2);
 
 points1 = detectSURFFeatures( im1 );
 features1 = extractFeatures( im1,points1 );
 
-points2 = detectSURFFeatures( im2);
-features2 = extractFeatures( im2,points1 );
+points2 = detectSURFFeatures( im2 );
+features2 = extractFeatures( im2,points2 );
 
-indexPairs = matchFeatures( features1, features2, ’Unique’, true );
+indexPairs = matchFeatures( features1, features2, "Unique", true );
 
 matchedPoints1 = points1( indexPairs( :,1 ) );
 matchedPoints2 = points2( indexPairs( :,2 ) );
 
-im1_points = matchedPoints1.Location ;
+im1_points = matchedPoints1.Location;
 im2_points = matchedPoints2.Location ;
 
-A=estimateTransform( im1_points, im2_points );
+%%A=estimateTransform( im1_points, im2_points );
