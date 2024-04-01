fb0415b0d90782d378c2c12c62884e67d07f7719
Trevor <trevoratorsavage1@gmail.com>
2024-04-01 16:29:49 -0400

    assignment 3 beginning

--- Assignment 3/Assignment_3.m
+++ Assignment 3/Assignment_3.m
@@ -1,1 +1,10 @@
-open isinfront.p
+load dalekosaur/object.mat
+
+patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+axis vis3d;
+axis equal;
+xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
+
+ObjectDirectory = ' dalekosaur';
+
+[impoints, objpoints3D] = clickPoints( InputImage1, ObjectDirectory );
