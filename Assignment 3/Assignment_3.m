bf9c6d38ca3862f61a842bf6d0ebdc153ddf8787
Trevor <trevoratorsavage1@gmail.com>
2024-04-02 17:33:08 -0400

    slightly farther

--- Assignment 3/Assignment_3.m
+++ Assignment 3/Assignment_3.m
@@ -5,6 +5,20 @@ axis vis3d;
 axis equal;
 xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
 
-ObjectDirectory = ' dalekosaur';
+ObjectDirectory = 'dalekosaur';
+InputImage = imread("InputImage1.jpg");
+InputImage = rgb2gray(InputImage);
+InputImage = im2double(InputImage);
 
-[impoints, objpoints3D] = clickPoints( InputImage1, ObjectDirectory );
+  
+[impoints2D, objpoints3D] = clickPoints( InputImage, ObjectDirectory );
+
+figure;
+imshow(I); hold on;
+plot( impoints2D(:,1), impoints2D(:,2), 'b.');
+
+figure;
+patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+axis vis3d;
+axis equal;
+plot3( objpoints3D(:,1), objpoints3D(:,2), objpoints3D(:,3), 'b.' );
